e8764cbfe0ff826224f316512f91b83deec6ef9d
MoonBowels <64305879+MoonBowels@users.noreply.github.com>
2026-06-26 13:28:23 -0400

    added frontmatter.ts, commands.ts

---
+++ src/commands.ts
@@ -0,0 +1,29 @@
+import { Notice, TFile } from "obsidian";
+import type MyPlugin from "./main";
+import { fmTemplater } from "./frontmatter";
+import { receipt } from "./helper";
+
+export function registerCommands(plugin: MyPlugin) {
+
+
+    plugin.addCommand({
+        id: 'fm-format',
+        name: 'Format current file YAML frontmatter',
+        callback: async () => {
+            
+            const file = plugin.app.workspace.getActiveFile()
+
+			if (!(file instanceof TFile)) {
+				new Notice('No active markdown file')
+				return
+			}
+
+            await plugin.app.fileManager.processFrontMatter(
+	            file, 
+	            fmTemplater
+            )
+
+            receipt('Frontmatter Formatted')
+        }
+    })
+}
---
+++ src/frontmatter.ts
@@ -0,0 +1,21 @@
+
+
+export const fmTemplater = (frontmatter: any)=>{
+	
+	// pull values into vars
+	const {
+		created: oldCreated, 
+		updated: oldUpdated
+	} = frontmatter
+	
+	// iterate and delete keys
+	for (const key of Object.keys(frontmatter)) {
+		delete frontmatter[key]
+	}
+	
+	frontmatter.created = oldCreated
+	frontmatter.updated = oldUpdated
+	frontmatter.aliases = []
+	frontmatter.tags    = []
+}
+
