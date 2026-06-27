ff550513721ba5c191fbc4f36bafcde8a181bad4
MoonBowels <64305879+MoonBowels@users.noreply.github.com>
2026-06-27 12:05:22 -0400

    cleaned header

--- src/header.ts
+++ src/header.ts
@@ -1,35 +1,14 @@
 import { Plugin } from "obsidian"
 import { TFile } from "obsidian"
-import {titlePattern, weekdayPattern, monthPattern} from './constants.ts'
+import {
+	titlePattern, weekdayPattern, monthPattern, 
+	flexibleHeaderRegex, meridiemRegex
+} from './constants.ts'
 import type { FullHeaderValues, MeridiemValues } from "./types.ts"
 
-export const flexibleHeaderRegex = new RegExp(
-	[
-		"^(?:\\r?\\n)?",                    // optional blank line at start
-		`(?<title>${titlePattern})`,        // title
-		"\\r?\\n",
 
-		// optional weekday: "Saturday, "
-		`(?:(?<weekday>${weekdayPattern}),\\s+)?`,
+// REGEX PROCESSING FUNCTIONS //
 
-		`(?<month>${monthPattern})`,        // month
-		"\\s+",
-		"(?<day>\\d{1,2})",                // day
-		",\\s+",
-		"(?<year>\\d{4})",                 // year
-		"\\r?\\n",
-
-		"(?<time>\\d{1,2}:\\d{2}\\s?(?:AM|PM))",
-		"(?:\\r?\\n){1,2}",
-	].join(""),
-	"d"
-)
-
-const meridiemRegex = /^(?:\r?\n)?(?<match>AM|PM)(?:\r?\n){1,2}/d
-
-
-
-// REGEX PROCESSING FUNCTIONS
 function withoutFrontmatter(text: string): string {
 	// Matches YAML frontmatter only if it starts at very beginning of file
 	const frontmatterRegex = /^---\r?\n[\s\S]*?\r?\n---\r?\n/
@@ -40,8 +19,6 @@ function withoutFrontmatter(text: string): string {
 export async function testRegex(plugin: Plugin, file: TFile) {
 	console.log('testregex')
 	const text = await plugin.app.vault.read(file)
-
-	
 
 }
 
